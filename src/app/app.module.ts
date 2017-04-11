4aa7b9a6ca88ccc6bf6eb6abf3c476248cf287d1
TheBirdyDeveloper <tehbirdy3@gmail.com>
2017-04-11 15:20:55 +0200

    Ajout du component "Folder" & début de l'API pour communication serveur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FolderComponent } from './folder/folder.component';
+import { DataAPIComponent } from './data-api/data-api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FolderComponent,
+    DataAPIComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dataAPI.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by Marco de Coco on 11/04/2017.
+ */
